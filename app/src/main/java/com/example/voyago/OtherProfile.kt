373810e4ddb50e965c55d3f62989e8711a66e1d0
FlavianaSchioppa <flavianaschioppa@gmail.com>
2025-04-04 12:28:57 +0200

    Scaffold, top and bottom bars

--- app/src/main/java/com/example/voyago/OtherProfile.kt
+++ app/src/main/java/com/example/voyago/OtherProfile.kt
@@ -9,15 +9,61 @@ import androidx.compose.material3.*
 //import androidx.compose.material.*
 import androidx.compose.animation.*
 import androidx.compose.foundation.layout.*
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.ui.graphics.*
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.unit.*
+import androidx.compose.ui.text.style.*
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun OtherProfile() {
 
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text("Top app bar")
+                }
+            )
+        },
+        bottomBar = {
+            BottomAppBar(
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                contentColor = MaterialTheme.colorScheme.primary,
+            ) {
+                Text(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    textAlign = TextAlign.Center,
+                    text = "Bottom app bar",
+                )
+            }
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+        ) {
+            Text(
+                modifier = Modifier.padding(8.dp),
+                text =
+                    """
+                    This is an example of a scaffold. It uses the Scaffold composable's parameters to create a screen with a simple top app bar, bottom app bar, and floating action button.
 
+                    It also contains some basic inner content, such as this text.
+                """.trimIndent(),
+            )
+        }
+    }
+    
+    /*
     //Column for all page
     Column(modifier =
     Modifier
@@ -62,9 +108,12 @@ fun OtherProfile() {
         {
 
         }
+        
+     
 
     }
 
 
+*/
 
 }
