76b1b86694fba7031cb711c05e18e612eb0ff229
FlavianaSchioppa <flavianaschioppa@gmail.com>
2025-04-06 16:11:52 +0200

    TopBar And Bottom Bar completed

--- app/src/main/java/com/example/voyago/MainActivity.kt
+++ app/src/main/java/com/example/voyago/MainActivity.kt
@@ -4,44 +4,67 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.voyago.ui.theme.VoyagoTheme
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            VoyagoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
+            val navController = rememberNavController()
+            NavHost(navController= navController, startDestination= "main_page", builder= {
+                composable("main_page"){
+                    MainPage(navController)
                 }
-            }
+                composable("user_profile") {
+                    UserProfileScreen()
+                }
+                composable("my_profile") {
+                    MyProfileScreen()
+                }
+                composable("travel_proposal") {
+                    TravelProposalScreen()
+                }
+            })
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    VoyagoTheme {
-        Greeting("Android")
+fun MainPage(navController: NavController) {
+    Column (
+        modifier = Modifier.fillMaxSize().padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = {
+            navController.navigate("my_profile")
+        }) {
+            Text("Go To Own Profile")
+        }
+        Button(onClick = {
+            navController.navigate("user_profile")
+        }) {
+            Text("Go To Other Profile")
+        }
+        Button(onClick = {
+            navController.navigate("travel_proposal")
+        }) {
+            Text("View Travel Proposal")
+        }
     }
 }
---
+++ app/src/main/java/com/example/voyago/UserProposalScreen.kt
@@ -0,0 +1,138 @@
+package com.example.voyago
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun UserProfileScreen() {
+
+    //Top Bar Images
+    val painterLogo = painterResource(R.drawable.logo)
+    val painterNews = painterResource(R.drawable.news)
+    val painterNotification = painterResource(R.drawable.notifications)
+
+    //Bottom Bar Images
+    val painterExplore = painterResource(R.drawable.explore)
+    val painterTrips = painterResource(R.drawable.trips)
+    val painterHome = painterResource(R.drawable.home)
+    val painterChat = painterResource(R.drawable.chat)
+    val painterProfile = painterResource(R.drawable.profile)
+
+    //NavBarItem
+    val navItemList = listOf(
+        NavItem(
+            "Explore", Image(
+                painter = painterExplore,
+                contentDescription = "explore"
+            ),
+            painter = painterExplore
+        ),
+        NavItem(
+            "My Trips", Image(
+                painter = painterTrips,
+                contentDescription = "trips"
+            ),
+            painter = painterTrips
+        ),
+        NavItem("Home", Image(
+            painter = painterHome,
+            contentDescription = "home"
+        ),
+            painter = painterHome
+        ),
+        NavItem("Chats", Image(
+            painter = painterChat,
+            contentDescription = "chats"
+        ),
+            painter = painterChat
+        ),
+        NavItem("Profile", Image(
+            painter = painterProfile,
+            contentDescription = "profile"
+        ),
+            painter = painterProfile
+        ),
+    )
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = topAppBarColors(
+                    containerColor = Color(0xe6, 0xe0, 0xe9, 255),
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Image(
+                        painter = painterLogo,
+                        contentDescription = "logo",
+                        modifier = Modifier.size(120.dp)
+                    )
+                },
+                actions = {
+                    IconButton(onClick = {/*TO DO*/}) {
+                        Image(
+                            painter = painterNews,
+                            contentDescription = "news",
+                            modifier = Modifier.padding(end = 10.dp)
+                        )
+                    }
+                    IconButton(onClick = {/*TO DO*/}) {
+                        Image(
+                            painter = painterNotification,
+                            contentDescription = "notification",
+                            modifier = Modifier.padding(end = 10.dp)
+                        )
+                    }
+                }
+            )
+        },
+        bottomBar = {
+            NavigationBar(
+                containerColor = Color(0xf3, 0xed, 0xf7, 255),
+                contentColor = MaterialTheme.colorScheme.primary,
+            ) {
+                navItemList.forEachIndexed { index, navItem ->
+                    NavigationBarItem(
+                        selected = false,
+                        onClick = {},
+                        icon = {
+                            Icon(
+                                navItem.painter,
+                                contentDescription = "Icon",
+                                modifier = Modifier.size(30.dp)) },
+                        label = {
+                            Text(text = navItem.label)
+                        }
+                    )
+                }
+            }
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+
+        }
+    }
+}
