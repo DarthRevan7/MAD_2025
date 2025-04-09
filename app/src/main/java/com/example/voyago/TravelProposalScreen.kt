08acf8fa7b6be88c9610f97d14bcca1592864bff
Sofia Suesdek <sofia.rocha@usp.br>
2025-04-09 19:17:28 -0300

    Ugly Trip view with scroll photo, itinerary

--- app/src/main/java/com/example/voyago/TravelProposalScreen.kt
+++ app/src/main/java/com/example/voyago/TravelProposalScreen.kt
@@ -1,10 +1,20 @@
 package com.example.voyago
 
+import android.graphics.Paint
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -13,10 +23,13 @@ import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.tooling.preview.Preview
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -91,12 +104,103 @@ fun TravelProposalScreen() {
             }
         }
     ) { innerPadding ->
-        Column(
+        val listState = rememberLazyListState()
+        LazyColumn(
+            state = listState,
             modifier = Modifier
+                .fillMaxSize()
                 .padding(innerPadding),
-            verticalArrangement = Arrangement.spacedBy(16.dp)
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.Start
         ) {
+            item {
 
+                Image(
+                    painter = painterResource(R.drawable.rome_photo),
+                    contentDescription = "Image before texts",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(200.dp)
+                        .padding(bottom = 16.dp)
+                )
+            }
+
+            item {
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(start = 16.dp, end = 16.dp)
+                ) {
+                    Text(
+                        text = "Left Aligned Text",
+                        modifier = Modifier.align(Alignment.CenterVertically)
+                    )
+                    Spacer(modifier = Modifier.weight(1f))
+                    Text(
+                        text = "Right Aligned Text",
+                        modifier = Modifier.align(Alignment.CenterVertically)
+                    )
+                }
+            }
+
+            item {
+                ItineraryTitleBox()
+            }
+
+            items(3) { index ->
+                ItineraryText(
+                    modifier = Modifier
+                        .padding(start = 24.dp, top = 16.dp)
+                )
+            }
         }
     }
+}
+
+
+@Composable
+fun ItineraryTitleBox() {
+    Box(
+        modifier = Modifier
+            .height(60.dp)
+            .fillMaxWidth()
+            .background(Color.Blue)
+    ) {
+        Text(
+            text = "My Itinerary",
+            color = Color.White,
+            modifier = Modifier
+                .align(Alignment.CenterEnd)
+                .padding(end = 16.dp)
+        )
+    }
+}
+
+@Composable
+fun ItineraryText(modifier: Modifier = Modifier) {
+    Text(
+        text = "Day one - \n lorem ipsum..." +
+                "Lorem ipsum dolor sit amet, consectetur \n" +
+                "adipisci elit, sed eiusmod tempor incidunt \n " +
+                "ut labore et dolore magna aliqua. \n" +
+                "Ut enim ad minim veniam, \n" +
+                "quis nostrum exercitationem \n " +
+                "ullam corporis suscipit\n" +
+                " laboriosam, nisi ut aliquid \n" +
+                " ex ea commodi consequatur.\n " +
+                "Quis aute iure reprehenderit in \n" +
+                "voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        style = MaterialTheme.typography.bodySmall,
+        fontWeight = FontWeight.Bold,
+        modifier = modifier
+
+    )
+}
+
+
+@Preview
+@Composable
+fun TravelProposalScreenPreview() {
+    TravelProposalScreen()
 }
