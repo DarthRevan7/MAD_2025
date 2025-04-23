e4faa62065651378df2191a92e59e0cd84e09591
Sofia Suesdek <sofia.rocha@usp.br>
2025-04-22 21:20:58 -0300

    Date selection

--- app/src/main/java/com/example/voyago/view/NewActivity.kt
+++ app/src/main/java/com/example/voyago/view/NewActivity.kt
@@ -1,6 +1,8 @@
 package com.example.voyago.view
 
 
+import android.app.DatePickerDialog
+import android.widget.DatePicker
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -19,22 +21,27 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.voyago.activities.*
+import java.util.Calendar
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -45,6 +52,7 @@ fun NewActivity(navController: NavController) {
     var activityTitle by rememberSaveable { mutableStateOf("") }
     var activityDescription by rememberSaveable { mutableStateOf("") }
 
+    var activityDate by rememberSaveable { mutableStateOf("") }
 
     Scaffold(
         topBar = {
@@ -90,6 +98,45 @@ fun NewActivity(navController: NavController) {
 
                 item {
                     Spacer(modifier = Modifier.height(50.dp))
+                }
+
+                item {
+                    val context = LocalContext.current
+                    val calendar = Calendar.getInstance()
+                    val year = calendar.get(Calendar.YEAR)
+                    val month = calendar.get(Calendar.MONTH)
+                    val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+
+
+                    val startDatePickerDialog = remember {
+                        DatePickerDialog(
+                            context,
+                            { _: DatePicker, y: Int, m: Int, d: Int ->
+                                activityDate = "$d/${m + 1}/$y"
+                            }, year, month, day
+                        )
+                    }
+
+
+                    Column(
+                        horizontalAlignment = Alignment.Start,
+                        modifier = Modifier
+                            .padding(vertical = 8.dp)
+                    ) {
+                        OutlinedButton(onClick = { startDatePickerDialog.show() }) {
+                            if (activityDate.isNotEmpty()){
+                                Text("Date: $activityDate")
+                            } else {
+                                Text("Select date")
+                            }
+
+                        }
+
+//                        if (activityDate.isNotEmpty()) {
+//                            Text("Start: $activityDate", modifier = Modifier.padding(top = 8.dp))
+//                        }
+                    }
                 }
 
                 item{
